7ac6d751a29b6855047f68239fec14e3c28798fe
vitorzanoni <vitorzanonib@gmail.com>
2022-02-20 16:32:58 -0300

    refatoracao

--- tcc.py
+++ tcc.py
@@ -2,93 +2,116 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pydicom
 from skimage import morphology
-from PIL import Image
+from PIL import Image, ImageOps
+import cv2
 
-image_original = pydicom.dcmread("CT000134.dcm")
-slope = image_original.RescaleSlope
-intercept = image_original.RescaleIntercept
-image_original = image_original.pixel_array
-image_original = image_original.astype(float)
-image_original = image_original * slope + intercept
+def getImagem(dicom):
+    dicom = pydicom.dcmread(dicom)
+    slope = dicom.RescaleSlope
+    intercept = dicom.RescaleIntercept
+    imagem = dicom.pixel_array
+    imagem[imagem == -2000] = 0
+    imagem = imagem.astype(float)
+    imagem = imagem * slope + intercept
+    return imagem
 
-plt.imshow(image_original, 'gray')
-plt.show()
+def mostraImagem(imagem):
+    plt.imshow(imagem, 'gray', vmin = 0, vmax = 255)
+    plt.show()
 
-final_image_original = Image.fromarray(image_original)
-plt.imshow(final_image_original, 'gray')
-plt.show()
+def converteEscalaCinza(imagem):
+    imagem = Image.fromarray(imagem)
+    # plt.imshow(imagem, 'gray')
+    # plt.show()
+    imagem_255 = ImageOps.grayscale(imagem)
+    imagem_255.save("cinza.png")
+    # plt.imshow(imagem_255, 'gray')
+    # plt.show()
+    return imagem_255
 
-#connected components opencv
-#fazer erosao e depois dilatacao
-image = pydicom.dcmread("CT000134.dcm")
-slope = image.RescaleSlope
-intercept = image.RescaleIntercept
+def mostraEscalaCinza(imagem):
+    plt.ylim([0, 20000])
+    plt.hist(imagem.getdata(), 255, color='c')
+    plt.xlabel("Escala de Cinza")
+    plt.ylabel("Frequência")
+    plt.show()
 
-image = image.pixel_array
-image[image == -2000] = 0
-image = image.astype(float)
-image = image * slope + intercept
+def mostraHounsfield(imagem):
+    plt.ylim([0, 2500])
+    plt.hist(imagem.flatten(), bins=100, color='c')
+    plt.xlabel("Unidades de Hounsfield (HU)")
+    plt.ylabel("Frequência")
+    plt.show()
+
+def segmentacao(imagem):
+    imagem[imagem <= 55] = 0
+    imagem[imagem >= 100] = 0
+    # plt.ylim([0, 2500])
+    # plt.hist(imagem.flatten(), bins=100, color='c')
+    # plt.xlabel("Unidades de Hounsfield (HU)")
+    # plt.ylabel("Frequência")
+    # plt.show()
+    return imagem
+
+def erosao(imagem):
+    imagem = morphology.erosion(imagem, morphology.disk(8)).astype(np.uint8)
+    plt.imshow(imagem, 'gray')
+    plt.show()
+    return imagem
+
+def dilatacao(imagem):
+    imagem = morphology.binary_dilation(imagem, morphology.disk(8)).astype(np.uint8)
+    plt.imshow(imagem, 'gray')
+    plt.show()
+    return imagem
+
+def aplicaMascara(imagem, mascara):
+    plt.imshow(mascara*imagem, 'gray')
+    plt.show()
+
+def componentesConectados(imagem):
+    imagem = cv2.imread(imagem, 0)
+    imagem = cv2.threshold(imagem, 200, 255, cv2.THRESH_BINARY)[1]
+    num_labels, labels = cv2.connectedComponents(imagem)
+    label_hue = np.uint8(179*labels/np.max(labels))
+    blank_ch = 255*np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+    labeled_img[label_hue==0] = 0
+    plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
+    plt.show()
+    plt.imshow(cv2.cvtColor(labeled_img, cv2.COLOR_BGR2RGB))
+    plt.show()
+
+# def plotMultiplasImagens(imagem1, imagem2, imagem3, imagem4):
+#     fig, ax = plt.subplots(2, 2, figsize=[12, 12])
+#     ax[0, 0].set_title("1")
+#     ax[0, 0].imshow(imagem1, 'gray')
+#     ax[0, 0].axis('off')
+#     ax[0, 1].set_title("2")
+#     ax[0, 1].imshow(imagem2, 'gray')
+#     ax[0, 1].axis('off')
+#     ax[1, 0].set_title("3")
+#     ax[1, 0].imshow(imagem3, 'gray')
+#     ax[1, 0].axis('off')
+#     ax[1, 1].set_title("4")
+#     ax[1, 1].imshow(imagem4, 'gray')
+#     ax[1, 1].axis('off')
+#     plt.show()
+
+# connected components opencv
+# fazer erosao e depois dilatacao
 # multiplicar slope e somar intercept, segmentacao -> remover de -200 e +1000
-
-# plt.hist(image.flatten(), bins=50, color='c')
-# plt.xlabel("Hounsfield Units (HU)")
-# plt.ylabel("Frequency")
-# plt.show()
-
-image[image <= 55] = 0
-image[image >= 100] = 0
-plt.hist(image.flatten(), bins=50, color='c')
-plt.xlabel("Hounsfield Units (HU)")
-plt.ylabel("Frequency")
-plt.show()
-
-plt.imshow(image, 'gray')
-plt.show()
-
-# final_image = Image.fromarray(image)
-# plt.imshow(final_image, 'gray')
-# plt.show()
-
-image_erosion = morphology.erosion(image, morphology.disk(5)).astype(np.uint8)
-image_erosion_2 = morphology.erosion(image, morphology.disk(8)).astype(np.uint8)
-
-plt.imshow(image_erosion, 'gray')
-plt.show()
-
-plt.imshow(image_erosion_2, 'gray')
-plt.show()
-
-image_dilation_1 = morphology.binary_dilation(image_erosion_2, morphology.square(8)).astype(np.uint8)
-image_dilation_2 = morphology.binary_dilation(image_erosion_2, morphology.diamond(8)).astype(np.uint8)
-image_dilation_3 = morphology.binary_dilation(image_erosion_2, morphology.disk(8)).astype(np.uint8)
-image_dilation_4 = morphology.binary_dilation(image_erosion_2, morphology.star(8)).astype(np.uint8)
-
-fig, ax = plt.subplots(2, 2, figsize=[12, 12])
-ax[0, 0].set_title("square")
-ax[0, 0].imshow(image_dilation_1, 'gray')
-ax[0, 0].axis('off')
-ax[0, 1].set_title("diamond")
-ax[0, 1].imshow(image_dilation_2, 'gray')
-ax[0, 1].axis('off')
-ax[1, 0].set_title("disk")
-ax[1, 0].imshow(image_dilation_3, 'gray')
-ax[1, 0].axis('off')
-ax[1, 1].set_title("star")
-ax[1, 1].imshow(image_dilation_4, 'gray')
-ax[1, 1].axis('off')
-plt.show()
-
-fig, ax = plt.subplots(2, 2, figsize=[12, 12])
-ax[0, 0].set_title("square")
-ax[0, 0].imshow(image_dilation_1*image_original, 'gray')
-ax[0, 0].axis('off')
-ax[0, 1].set_title("diamond")
-ax[0, 1].imshow(image_dilation_2*image_original, 'gray')
-ax[0, 1].axis('off')
-ax[1, 0].set_title("disk")
-ax[1, 0].imshow(image_dilation_3*image_original, 'gray')
-ax[1, 0].axis('off')
-ax[1, 1].set_title("star")
-ax[1, 1].imshow(image_dilation_4*image_original, 'gray')
-ax[1, 1].axis('off')
-plt.show()
+def main():
+    imagem = getImagem("CT000134.dcm")
+    mostraImagem(imagem)
+    mostraHounsfield(imagem)
+    imagem_255 = converteEscalaCinza(imagem)
+    mostraEscalaCinza(imagem_255)
+    imagem_seg = segmentacao(imagem)
+    mostraImagem(imagem_seg)
+    imagem_seg = erosao(imagem_seg)
+    imagem_seg = dilatacao(imagem_seg)
+    aplicaMascara(imagem, imagem_seg)
+    componentesConectados("cinza.png")
+main()
