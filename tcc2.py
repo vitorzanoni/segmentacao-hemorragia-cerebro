bb86321d42cbead7958d845a254b0e244daef85e
vitorzanoni <vitorzanonib@gmail.com>
2022-10-26 18:19:09 -0300

    bateria de testes pronta

--- tcc2.py
+++ tcc2.py
@@ -77,39 +77,51 @@ def aplicaMascara(imagem, mascara):
     plt.show()
 
 
-def componentesConectados(imagem):
-    # imagem = cv2.imread(imagem, 0)
+def componentesConectados(imagem, seg_max, cc_erosion):
+    original = window_image(imagem, 60, 120).astype(np.uint8)
+    imagem[imagem > seg_max] = 0
+    imagem = imagem * \
+        morphology.binary_opening(imagem, morphology.disk(cc_erosion))
     imagem = window_image(imagem, 60, 120).astype(np.uint8)
-    # img_min = 60 - 120//2  # minimum HU level
-    # img_max = 60 + 120//2  # maximum HU level
-    # min = (60 - img_min) / (img_max - img_min)*255.0
-    # max = (90 - img_min) / (img_max - img_min)*255.0
-    # num_labels, labels = cv2.connectedComponents(imagem.astype(np.uint8))
     nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(
         imagem)
     sizes = stats[:, -1]
-    max_label = 1
-    max_size = sizes[1]
-    for i in range(2, nb_components):
-        if sizes[i] > max_size:
-            max_label = i
-            max_size = sizes[i]
-    img2 = np.zeros(output.shape)
-    img2[output == max_label] = 1
-    # cv2.imshow("Biggest component", img2)
-    # cv2.waitKey()
-
-    # img2 = np.zeros(output.shape)
-    # img2[output == max_label] = 255
-    # label_hue = np.uint8(255*labels/np.max(labels))
-    # blank_ch = 255*np.ones_like(label_hue)
-    # labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
-    # labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
-    # labeled_img[label_hue == 0] = 0
-    # plt.imshow(cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB))
-    # plt.show()
-    # plt.imshow(cv2.cvtColor(labeled_img, cv2.COLOR_BGR2RGB))
-    # plt.show()
+    if(len(sizes) > 1):
+        max_label = 1
+        max_size = sizes[1]
+        for i in range(2, nb_components):
+            if sizes[i] > max_size:
+                max_label = i
+                max_size = sizes[i]
+        img2 = np.zeros(output.shape)
+        img2[output == max_label] = 1
+    else:
+        img2 = imagem
+    antes = img2
+    img2 = morphology.binary_closing(img2, morphology.disk(cc_erosion))
+    depois = img2
+    original = original * depois
+    antes = antes * 255
+    depois = depois * 255
+    teste = depois - antes
+    antes = cv2.cvtColor(antes.astype(np.uint8), cv2.COLOR_GRAY2BGR)
+    depois = cv2.cvtColor(depois.astype(np.uint8), cv2.COLOR_GRAY2BGR)
+    teste = cv2.cvtColor(teste.astype(np.uint8), cv2.COLOR_GRAY2BGR)
+    original = cv2.cvtColor(original, cv2.COLOR_GRAY2BGR)
+    seg1 = np.concatenate((antes, depois), axis=1)
+    seg2 = np.concatenate((teste, original), axis=1)
+    seg3 = np.concatenate((seg1, seg2), axis=0)
+    cv2.imshow('Teste', seg3)
+    if (cv2.waitKey(1) & 0xFF) == ord('q'):
+        cv2.destroyAllWindows()
+    for i in range(len(img2)):
+        if(1 in img2[i]):
+            img2[i:i + round(len(img2)*0.05), :] = 0
+            break
+    for i in range(len(img2) - 1, 0, -1):
+        if(1 in img2[i]):
+            img2[i - round(len(img2)*0.05):, :] = 0
+            break
     return img2
 
 
@@ -173,74 +185,59 @@ def findImagens(diretorio):
     return imagens
 
 
-def teste(imagem):
-    img = window_image(imagem, 60, 120).astype(np.uint8)
-
-    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(
-        img)
-    sizes = stats[:, -1]
-    max_label = 1
-    max_size = sizes[1]
-    for i in range(2, nb_components):
-        if sizes[i] > max_size:
-            max_label = i
-            max_size = sizes[i]
-    img2 = np.zeros(output.shape)
-    img2[output == max_label] = 1
-    cv2.imshow("Biggest component", img2)
-
-    # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    cv2.imshow("gray", img)
-
-    blur = cv2.medianBlur(img, 31)
-    cv2.imshow("blur", blur)
-    plt.imshow(blur, vmin=0, vmax=255)
-    plt.show()
-
-    ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_OTSU)
-    cv2.imshow("thresh", thresh)
-
-    canny = cv2.Canny(img, 75, 200)
-    cv2.imshow('canny', canny)
-
-    borda = cv2.dilate(thresh, None) - thresh
-    cv2.imshow('borda', borda)
-    cv2.waitKey()
-
-    # im2, contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-    # contour_list = []
-    # for contour in contours:
-    #     approx = cv2.approxPolyDP(contour, 0.01 * cv2.arcLength(contour, True), True)
-    #     area = cv2.contourArea(contour)
-    #     if 5000 < area < 15000:
-    #         contour_list.append(contour)
-
-    # msg = "Total holes: {}".format(len(approx)//2)
-    # cv2.putText(img, msg, (20, 40), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2, cv2.LINE_AA)
-
-    # cv2.drawContours(img, contour_list, -1, (0, 255, 0), 2)
-    # cv2.imshow('Objects Detected', img)
-
-    # cv2.imwrite("detected_holes.png", img)
-
-    # cv2.waitKey(0)
+def load_scan_sorted(list_dicom_paths):
+    slices = []
+    for path in list_dicom_paths:
+        slices.append([path, pydicom.dcmread(path)])
+    slices = sorted(slices, key=lambda x: x[1].ImagePositionPatient[-1])
+    imagens = []
+    for slice in slices:
+        imagens.append(slice[0])
+    imagens = imagens[round(len(imagens)*0.4):]
+    return imagens[:round(len(imagens)*0.8)]
 
 
-def findHemorragia(imagens, diretorio, seg_min, seg_max, disk_size):  # , area_size
+def findHemorragia(imagens, diretorio, seg_min, seg_max, disk_size, cc_erosion):  # , area_size
     # if os.path.basename(diretorio) == "CQ500CT100 CQ500CT100":
     fw = open("tcc_teste.csv", "a")
     count = 0
+    imagens = load_scan_sorted(imagens)
+    print("cc_erosion -> %d" % cc_erosion)
+    print(diretorio)
     for i in imagens:
         imagem = getImagem(i)
+        imagem = imagem * componentesConectados(imagem, seg_max, cc_erosion)
         imagem[imagem > seg_max] = 0  # 75
-        imagem = imagem * componentesConectados(imagem)
+        area = window_image(imagem, 60, 120).astype(np.uint8)
         imagem[imagem < seg_min] = 0  # 60
+        antes = window_image(imagem, 60, 120).astype(np.uint8)
         imagem = morphology.binary_opening(
             imagem, morphology.disk(disk_size))  # 3
         # imagem = morphology.area_opening(imagem, area_size)  # 50
         if imagem.max() > 0:
-            # original = getImagem(i)
+            count += 1
+            original = window_image(getImagem(i), 60, 120).astype(np.uint8)
+            original = cv2.cvtColor(original, cv2.COLOR_GRAY2BGR)
+            imagem = window_image(imagem, 60, 120).astype(np.uint8)
+            depois = imagem * 255
+            borda = cv2.dilate(imagem, None) - imagem
+            contours = cv2.findContours(
+                borda, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[0]
+            cv2.drawContours(original, contours, -1, (0, 0, 255), 1)
+            msg = str(count)
+            cv2.putText(original, msg, (20, 40),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2, cv2.LINE_AA)
+            area = cv2.cvtColor(area, cv2.COLOR_GRAY2BGR)
+            antes = cv2.cvtColor(antes, cv2.COLOR_GRAY2BGR)
+            depois = cv2.cvtColor(depois, cv2.COLOR_GRAY2BGR)
+            seg1 = np.concatenate((area, antes), axis=1)
+            seg2 = np.concatenate((depois, original), axis=1)
+            seg3 = np.concatenate((seg1, seg2), axis=0)
+            cv2.imshow('Segmentacao', seg3)
+            if (cv2.waitKey(1) & 0xFF) == ord('q'):
+                cv2.destroyAllWindows()
+                break
+
             # imagem = original
             # imagem[imagem > 75] = 0
             # teste(imagem)
@@ -298,7 +295,6 @@ def findHemorragia(imagens, diretorio, seg_min, seg_max, disk_size):  # , area_s
             # plt.title("Binary Open 3")
             # plt.show()
 
-            count += 1
             # print(i)
     if count >= 1:
         fw.write(os.path.basename(diretorio) + ";1;" + str(count) + "\n")
@@ -309,16 +305,14 @@ def findHemorragia(imagens, diretorio, seg_min, seg_max, disk_size):  # , area_s
     return 0
 
 
-RES = [0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0,
-       0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0]
+RES = [0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+       0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0]
 diretorios = findDiretorios()
 # print(diretorios)
 
 
 def performance(bounds):
-    seg_min, seg_max, disk_size = bounds  # , area_size
-    f1txt = open("resultados.txt", "a")
-    f1txt.write("%d, %d, %d\n" % (seg_min, seg_max, disk_size))
+    seg_min, seg_max, disk_size, cc_erosion = bounds  # , area_size
     # print("%d, %d, %d" % (seg_min, seg_max, disk_size))
     fw = open("tcc_teste.csv", "w")
     fw.write("PASTA;RES\n")
@@ -328,15 +322,17 @@ def performance(bounds):
     res = []
     i = 0
     for diretorio in diretorios:
-        if (os.path.isdir(diretorio)) and (len(diretorios_teste) < 20):
+        if (os.path.isdir(diretorio)) and (len(diretorios_teste) < 40):
             diretorios_teste.append(diretorio)
             validacao.append(RES[i])
             res.append(findHemorragia(findImagens(diretorio),
-                       diretorio, seg_min, seg_max, disk_size))
+                       diretorio, seg_min, seg_max, disk_size, cc_erosion))
         i += 1
     # print(validacao)
     # print(res)
     score = f1_score(validacao, res)
+    f1txt = open("resultados.txt", "a")
+    f1txt.write("%d, %d, %d, %d\n" % (seg_min, seg_max, disk_size, cc_erosion))
     f1txt.write("F1-Score: %.2f\n" % (score))
     f1txt.close()
     # print("F1-Score:", score)
@@ -344,10 +340,9 @@ def performance(bounds):
 
 
 def main():
-    bounds = [(60, 75), (75, 91), (2, 6)]  # , (0, 1000)
+    bounds = [(50, 75), (75, 101), (2, 6), (2, 11)]  # , (0, 1000)
     result = differential_evolution(performance, bounds)
-    print(result.x)
-    print(result.fun)
+    print(result)
 
 
 main()
